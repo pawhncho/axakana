402df0b5ca3258e76cd94441754bd0eeb0f85366
pawhncho <pawhncho@gmail.com>
2025-06-26 14:14:25 -0700

    First Commit

--- server/settings.py
+++ server/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': '5432',
+        'PORT': 5432,
     }
 }
 
